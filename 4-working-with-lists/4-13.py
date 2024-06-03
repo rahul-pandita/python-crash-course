d456ddf7376c813d77fbedb9ed813aa6104fb64a
Rahul Pandita <rpandita1999@outlook.com>
2024-06-03 09:55:16 +0530

    exercise 4-13 complete

---
+++ 4-working-with-lists/4-13.py
@@ -0,0 +1,16 @@
+# BUFFET
+# Think of five basic foods and store them in a tuple
+# Use a for loop to print each food the restaurant offers
+# relace two items - add a line that rewrites the tuples
+
+menu = ("pizza", "burger", "salad", "roti", "fish")
+
+for item in menu:
+    print(item)
+
+# menu[0] = "sandwich"
+
+menu = "sandwich", "maggi", "salad", "roti", "fish"
+
+for item in menu:
+    print(item)
